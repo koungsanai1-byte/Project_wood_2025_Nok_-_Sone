500fe8aa276e42daeea44cbb4c2f363fa6f856cc
koungsanai1 <koungsanai1@gmail.com>
2025-08-04 01:29:07 +0700

    three

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Project/",
+  base: "/Project_wood_2025_Nok_-_Sone/",
   plugins: [react()],
 })
